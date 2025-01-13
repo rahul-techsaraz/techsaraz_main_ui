a86e0291f25e2547006836d5589ffb2ed790b5d7
Adarsh-Techsaraz <155561524+Adarsh-Techsaraz@users.noreply.github.com>
2025-01-13 20:24:43 +0530

    Swiper implemented and testimonials populated with data (#5)
    * Added Images
    
    * Footer conflict fixed
    
    * Swiper implemented and testemonials populated with data
    
    ---------
    
    Co-authored-by: Adarsh-MERN <singhadarsh999@gmail.com>

---
+++ src/Components/CustomSwiper/CustomSwiper.js
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react'
+import {register} from "swiper/element/bundle"
+
+export default function CustomSwiper({noOfSlidesPerView, children, isBreakPoint, navigationNext, navigationPrev, isPagination, breakPoint}) {
+    useEffect(()=>{
+        register()
+    },[])
+    
+  return (
+    <>
+    {isBreakPoint ? (<swiper-container slides-per-view={noOfSlidesPerView} space-between={20} breakpoints={JSON.stringify(breakPoint)} autoplay-delay={2000} pauseOnMouseEnter={true}>{children}</swiper-container>) : 
+    <swiper-container navigation-next-el={navigationNext} navigation-prev-el={navigationPrev} slides-per-view={noOfSlidesPerView} space-between={20} navigation={true} autoplay-delay={2000} pauseOnMouseEnter={true}>{children}</swiper-container>}
+    </>
+  )
+}
--- src/Pages/Services/TestimonialSection.js
+++ src/Pages/Services/TestimonialSection.js
@@ -1,11 +1,24 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { images } from '../../Constant/images';
 import CustomOwlCarousel from '../../Components/CustomOwlCarousel/CustomOwlCarousel';
+import CustomSwiper from '../../Components/CustomSwiper/CustomSwiper';
+import { SwiperResponsive } from '../../Components/CustomSwiper/SwiperResponsive';
+import { constants } from '../../Constant/constants';
+import { customerRivew } from '../../Constant/config';
+import { reviewExpander } from '../../Utils/reviewExpander';
 
 
 const TestimonialSection = () => {
     const {quotesImage,
         pexelsnkhajotia1516680Image} = images;
+	const [expandedReviews, setExpandedReviews] = useState({});
+
+	const toggleReview = (index) => {
+		setExpandedReviews((prevState) => ({
+		  ...prevState,
+		  [index]: !prevState[index],
+		}));
+	  };
 
         
   return (
@@ -19,7 +32,7 @@ const TestimonialSection = () => {
 		<h1 className="testmonial-h1">What Clients Say</h1>
     </div>
 	{/* <div className="owl-carousel owl-theme testimonials-container"> */} 
-        <CustomOwlCarousel isloop={true} margin={10} isnav={true}>
+        {/* <CustomOwlCarousel isloop={true} margin={10} isnav={true}>
     	<div className="item testimonial-card">
     		<main className="test-card-body">
     			<div className="quote">
@@ -175,7 +188,51 @@ const TestimonialSection = () => {
     			</div>
     		</div>
     	</div>
-        </CustomOwlCarousel>
+        </CustomOwlCarousel> */}
+		 <div className="swiper-wrapper position-relative">
+			<CustomSwiper navigationNext='.courselist-button-next' navigationPrev='.courselist-button-prev' noOfSlidesPerView={2} isBreakPoint={true} breakPoint={SwiperResponsive()}>
+				{customerRivew.map((review, index)=>(
+					<swiper-slide>
+					<div className="swiper-slide position-relative">
+						<div className="item testimonial-card">
+							<main className="test-card-body">
+								{/* <div className="quote">
+									<i className="fa fa-quote-left"></i>
+									<h2>{review.quote}</h2>
+								</div> */}
+								<p className="testimonial-p">{expandedReviews[index]
+              ? review?.review
+              : `${review?.review.slice(0, 100)}...`}</p>
+								<button onClick={()=>toggleReview(index)}>{expandedReviews[index] ? 'Read Less' : 'Read More'}</button>
+								<div className="ratings">
+									{Array.from({ length: 5 }, (_, index) => (
+										<i	key={index}	className={`fa-solid fa-star ${index < review?.stars ? 'filled' : ''}`}
+										></i>
+									))}
+								</div>
+							</main>
+							<div className="profile">
+								<div className="profile-image">
+									<img src={review.profileImage} alt='' />
+								</div>
+								<div className="profile-desc">
+									<span>{review.name}</span>
+									<span>{review.date}</span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</swiper-slide>
+				))}
+				
+			</CustomSwiper>
+			<div class="swiper-button-prev courselist-button-prev">
+				{/* <img src={leftArrow} alt="" /> */}
+			</div>
+			<div class="swiper-button-next courselist-button-next">
+				{/* <img src={rightArrow} alt="" /> */}
+			</div>
+		</div> 
     {/* </div> */}
 </div>
 
