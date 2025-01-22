f6a430e11c9e31c73b38a49503c3d81cc8d05df8
Adarsh-MERN <singhadarsh999@gmail.com>
2025-01-22 18:12:51 +0530

    WhatsUp implemented at App level

---
+++ src/Pages/LandingPages/WhatsUp.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import whatsappicon from '../../images/whatsapp-button-image.png'
+import { Link } from 'react-router-dom'
+
+const WhatsUp = () => {
+  return (
+    <Link to='https://wa.me/+917760231920' target='blank'>
+      <div className='whatsapp-button'>
+        <img src={whatsappicon} alt='whatsapp-icon'/>
+      </div>
+    </Link>
+  )
+}
+
+export default WhatsUp
